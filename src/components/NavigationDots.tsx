927b69c54c8a3b104b129243f9dc15e30046462c
sss1stclass <sohilkr88@gmail.com>
2023-03-05 22:18:03 +0530

    minor changes for builds

--- src/components/NavigationDots.tsx
+++ src/components/NavigationDots.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 const types = ['home', 'about', 'work', 'skills', 'testimonial', 'contact']
+type NavigationDotsProps = {
+active:any;
+}
 
-const NavigationDots = ({active}) => (
+const NavigationDots: React.FC<NavigationDotsProps> = ({active}) => (
   <div className="app__navigation">
     {types.map((item, index) => (
       <a
         href={`#${item}`}
         key={item + index}
         className="app__navigation-dot"
-        style={active == item ? { backgroundColor: '#313BAC' } : {}}
+        style={active === item ? { backgroundColor: '#313BAC' } : {}}
       />
     ))}
   </div>
